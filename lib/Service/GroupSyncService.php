4c112369b142c12aabdfc3d40f165710bb948d99
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-08-26 10:05:33 +0200

    add remove from groups to group sync

--- lib/Service/GroupSyncService.php
+++ lib/Service/GroupSyncService.php
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
+
 namespace OCA\CesnetOpenIdConnect\Service;
 
 use OCA\CesnetOpenIdConnect\Db\GroupMapper;
@@ -33,7 +34,8 @@ use OCP\IUser;
 use OCP\IUserManager;
 use Tobias\Urn\RFC8141\Parser;
 
-class GroupSyncService {
+class GroupSyncService
+{
 
 	/**
 	 * @var Parser
@@ -60,12 +62,13 @@ class GroupSyncService {
 	 */
 	private $config;
 
-	public function __construct(IUserManager $userManager,
+	public function __construct(IUserManager  $userManager,
 								IGroupManager $groupManager,
-								GroupMapper $groupMapper,
-								ILogger $logger,
-								IConfig $config,
-								Parser $urnParser) {
+								GroupMapper   $groupMapper,
+								ILogger       $logger,
+								IConfig       $config,
+								Parser        $urnParser)
+	{
 		$this->userManager = $userManager;
 		$this->groupManager = $groupManager;
 		$this->groupMapper = $groupMapper;
@@ -74,52 +77,94 @@ class GroupSyncService {
 		$this->urnParser = $urnParser;
 	}
 
-	public function syncUserGroups($user, $userInfo) {
+	public function syncUserGroups($user, $userInfo)
+	{
 		if (!$this->enabled()) {
 			throw new LoginException('Group sync is disabled.');
+		}
+		if (!$user || !$userInfo) {
+			throw new LoginException('User data is missing.');
 		}
 
 		$groupsClaim = $this->groupsClaim();
 		if (!$groupsClaim) {
 			throw new LoginException('Groups claim must be configured for group sync.');
 		}
-		$groups = $userInfo->$groupsClaim;
-		foreach ($groups as $groupUrn) {
-			$groupAttrs = $this->urnParser->parse('urn:' . substr($groupUrn,4));
+		$groupURNs = $userInfo->$groupsClaim;
+		$externalGroups = array();
+
+		// Add user to newly added groups
+		foreach ($groupURNs as $groupURN) {
+			$groupAttrs = $this->urnParser->parse('urn:' . substr($groupURN, 4));
 			$gNS = $groupAttrs->getNamespaceIdentifier();
 			$gNSS = $groupAttrs->getNamespaceSpecificString();
 			$gRQF = $groupAttrs->getRQF();
 
-			if (substr($gNSS, 0, strlen($this->groupsRealm())+1) === $this->groupsRealm() . ':') {
-				$gNSS = substr($gNSS, strlen($this->groupsRealm())+1);
+			if (substr($gNSS, 0, strlen($this->groupsRealm()) + 1) === $this->groupsRealm() . ':') {
+				$gNSS = substr($gNSS, strlen($this->groupsRealm()) + 1);
 				if (substr($gNSS, 0, strlen('group:')) === 'group:') {
-					$gid = substr($gNSS, strlen('group:')+1);
+					$gid = substr($gNSS, strlen('group:') + 1);
 					$this->logger->debug("Parsed group data: (NS: $gNS NSS: $gid)");
 
 					$group = $this->groupMapper->getGroupID($gid);
 					$g = $this->groupManager->get($group);
 					if ($g) {
-						$g->addUser($user);
+						$externalGroups[] = $g;
+						if (!$g->inGroup($user)) {
+							$this->logger->info("Adding: " . $user->getUID() . " to: ". $g->getGID());
+							$g->addUser($user);
+						}
+					} else {
+						$this->logger->warning("Group " . $group . "(" . $groupURN . ") doesn't exist. Skipping...");
 					}
 				}
 			} else {
 				$this->logger->debug("Skipping group " . $gNSS);
 			}
 		}
-	}
-	public function getOpenIdConfiguration(): array {
-		return $this->config->getSystemValue('openid-connect', null) ?? [];
+
+		$internalGroups = $this->groupManager->getUserGroupIds($user);
+		// Remove user from removed groups
+		$groupsToRemoveFrom = array_udiff($internalGroups, $externalGroups, array($this, 'compareGroups'));
+		$this->logger->debug("Groups to remove from " . $groupsToRemoveFrom);
+
+		foreach ($groupsToRemoveFrom as $g) {
+			$this->logger->info("Removing: " . $user->getUID() . " from: ". $g->getGID());
+			$g->removeUser($user);
+		}
 	}
 
-	public function enabled(): bool {
+	public function compareGroups($g1, $g2): int
+	{
+		$a = $g1->getGID();
+		$b = $g2->getGID();
+
+		if ($a == $b) {
+			return 0;
+		} elseif ($a > $b) {
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	public function enabled(): bool
+	{
 		return $this->getOpenIdConfiguration()['group-sync']['enabled'] ?? false;
 	}
 
-	private function groupsClaim() {
+	public function getOpenIdConfiguration(): array
+	{
+		return $this->config->getSystemValue('openid-connect', null) ?? [];
+	}
+
+	private function groupsClaim()
+	{
 		return $this->getOpenIdConfiguration()['group-sync']['groups-claim'] ?? 'eduperson_entitlement_extended';
 	}
 
-	private function groupsRealm() {
+	private function groupsRealm()
+	{
 		return $this->getOpenIdConfiguration()['group-sync']['groups-realm'] ?? 'cesnet.cz';
 	}
 }
