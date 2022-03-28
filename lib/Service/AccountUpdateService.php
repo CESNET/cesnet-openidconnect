5c09e67daff2cd7794a129484379509459954a51
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-03-28 19:04:09 +0200

    namespacing fix

--- lib/Service/AccountUpdateService.php
+++ lib/Service/AccountUpdateService.php
@@ -20,7 +20,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-namespace OCA\OpenIdConnect\Service;
+namespace OCA\CesnetOpenIdConnect\Service;
 
 use OCA\CesnetOpenIdConnect\Client;
 use OC\User\LoginException;
