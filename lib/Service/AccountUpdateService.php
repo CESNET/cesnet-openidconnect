98b54c2ba98d421ffc48f7d865feb77099cf9269
Miroslav Bauer <mirekys@users.noreply.github.com>
2022-03-28 21:31:54 +0200

    backwards compatibility

--- lib/Service/AccountUpdateService.php
+++ lib/Service/AccountUpdateService.php
@@ -62,7 +62,10 @@ class AccountUpdateService {
 		}
 		$autoupdated = $this->autoupdatedAttributes();
 
-		if ((\in_array('email', $autoupdated) && $user->canChangeMailAddress()) || $force) {
+		$this->logger->debug('AccountUpdateService::updateAccountInfo: user: ' . \json_encode($user));
+
+		if (force || (\in_array('email', $autoupdated) &&
+			(!\method_exists('canChangeMailAddress') || $user->canChangeMailAddress()))) {
 			$currentEmail = $this->client->getUserEmail($userInfo);
 			if ($currentEmail && $currentEmail !== $user->getEMailAddress()) {
 				$this->logger->debug('AccountUpdateService: updating e-mail to ' . $currentEmail);
@@ -70,7 +73,7 @@ class AccountUpdateService {
 			}
 		}
 
-		if ((\in_array('display-name', $autoupdated) && $user->canChangeDisplayName()) || $force) {
+		if ($force || (\in_array('display-name', $autoupdated) && $user->canChangeDisplayName())) {
 			$currentDN = $this->client->getUserDisplayName($userInfo);
 			if ($currentDN && $currentDN !== $user->getDisplayName()) {
 				$this->logger->debug('AccountUpdateService: updating display name to ' . $currentDN);
